2d7161111b239cb5fabf67fbd600ed7b34803aee
Alex Gesti - Pixel Cuak - Mr.Luigi001 <40181553+alexgesti@users.noreply.github.com>
2025-06-02 13:55:59 +0200

    js, part2 and footer functions

---
+++ functions.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Open just one tab by default
+  const tabcontent = document.getElementsByClassName("tab-content");
+  const buttons = document.getElementsByClassName("button-part2");
+  if (tabcontent.length > 0) {
+    tabcontent[0].style.display = "block";
+  }
+  if (buttons.length > 0) {
+    buttons[0].className += " active";
+  }
+
+  // Handles the email validation for the contact form
+  const emailInput = document.getElementById("email-input");
+  const emailError = document.getElementById("email-error");
+  const contactBtn = document.querySelector(".button-contact");
+
+  emailError.style.display = "none";
+  emailInput.addEventListener("input", function () {
+    const email = emailInput.value;
+    if (email.includes("@") && email.includes(".")) {
+      emailError.style.display = "none";
+      emailInput.classList.remove("error");
+    } else {
+      emailError.style.display = "flex";
+      emailInput.classList.add("error");
+      e.preventDefault();
+    }
+  });
+});
+
+// Tabs fuction.
+function openTag(event, tagName) {
+  let tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tab-content");
+  buttons = document.getElementsByClassName("button-part2");
+
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none"; // Hide all tab content
+  }
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].className = buttons[i].className.replace(" active", ""); // Remove active class from all tabs
+  }
+
+  document.getElementById(tagName).style.display = "block"; // Show the selected tab content
+  event.currentTarget.className += " active"; // Add active class to the clicked tab
+}
